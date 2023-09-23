013dcaf57e94cf34035f00e7dd493819c8a9fccc
Dinis Cruz <dinis.cruz@owasp.org>
2023-09-23 22:34:03 +0100

    added type definitions to Playwright_Page

--- osbot_playwright/playwright/Playwright_Page.py
+++ osbot_playwright/playwright/Playwright_Page.py
@@ -1,12 +1,15 @@
+from playwright.sync_api import BrowserContext, Page
+
 from osbot_playwright.html_parser.Html_Parser import Html_Parser
+from osbot_playwright.playwright.Playwright_Chrome_Browser import Playwright_Chrome_Browser
 
 
 class Playwright_Page:
 
     def __init__(self, browser, context, page):
-        self.browser = browser
-        self.context = context
-        self.page    = page
+        self.browser : Playwright_Chrome_Browser = browser
+        self.context : BrowserContext            = context
+        self.page    : Page                      = page
 
     def close(self):
         return self.page.close()
