51046090047676f64cf1ddab254e528538229053
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-29 02:17:17 +0000

    started refactoring docker playwright image to use the new one

--- osbot_playwright/docker/images/osbot_playwright/run-docker.sh
+++ osbot_playwright/docker/images/osbot_playwright/run-docker.sh
@@ -10,7 +10,7 @@ CONTAINER_PORT="8000"
 docker build -t $IMAGE_REPOSITORY/$IMAGE_NAME:$TAG .
 
 # Run the Docker container
-#docker run --rm -it -v $(pwd):/var/task -p $LOCAL_PORT:$CONTAINER_PORT $IMAGE_REPOSITORY/$IMAGE_NAME:$TAG
+docker run --rm -it -v $(pwd):/var/task -p $LOCAL_PORT:$CONTAINER_PORT $IMAGE_REPOSITORY/$IMAGE_NAME:$TAG
 
 # Example of another run command with the same image but different name and tag
 #docker run --rm -it -p $LOCAL_PORT:$CONTAINER_PORT $IMAGE_REPOSITORY/docker_playwright:$TAG
