162afb4c979ed048c559e7099435abfb1218bee3
Dinis Cruz <dinis.cruz@owasp.org>
2023-12-30 03:09:43 +0000

    wired up the Fast_API__Playwright code route

--- tests/unit/playwright/fastapi/test_Routes__Playwright.py
+++ tests/unit/playwright/fastapi/test_Routes__Playwright.py
@@ -1,8 +1,13 @@
+import os
 from unittest import TestCase
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from osbot_fast_api.utils.http_shell.Http_Shell__Server import ENV__HTTP_SHELL_AUTH_KEY
 from osbot_utils.utils.Dev import pprint
-from osbot_utils.utils.Misc import obj_info
+from osbot_utils.utils.Functions import function_source_code
+from osbot_utils.utils.Misc import obj_info, list_set, bytes_to_str
+from starlette.responses import HTMLResponse
 from starlette.testclient import TestClient
 
 from osbot_playwright.playwright.fastapi.Routes__Playwright import Routes__Playwright, ROUTES_PATHS__PLAYWRIGHT, \
@@ -15,21 +20,40 @@ class test_Routes__Playwright(TestCase):
         self.app =  FastAPI()
         self.routes_playwright = Routes__Playwright(self.app)
 
-    def test__client__code(self):
-        data = dict(auth_key ='123'  ,
-                    code     = 'abc' )
-        client = TestClient(self.app)
-        response = client.post('/playwright/code',json=data)
-        pprint(response.text)
+    def auth_key(self):
+        load_dotenv()
+        return os.environ.get(ENV__HTTP_SHELL_AUTH_KEY)
 
-    # def test_code(self):
-    #     code = 'abc'
-    #     assert self.routes_playwright.code(code) == code
+    def test_client_code(self):
+        expected_result = { 'result': { 'title': 'Error 404 (Not Found)!!1',
+                            'url'   : 'https://www.google.com/404'}        ,
+                            'status': 'ok'                                 }
+        def callback(browser):
+            page = browser.new_page()
+            page.goto('https://www.google.com/404')
+            url = page.url
+            title = page.title()
+            return { 'url': url, 'title': title }
+
+        method_code = function_source_code(callback)
+
+        data = dict(auth_key=self.auth_key(),
+                    code=method_code)
+        client = TestClient(self.app)
+        response = client.post('/playwright/code', json=data)
+        assert response.status_code == 200
+        assert response.json() == expected_result
+        return
+
 
     def test_html(self):
-        url    = 'https://httpbin.org/get'
-        result = self.routes_playwright.html(url)
-        pprint(result)
+        url      = 'https://httpbin.org/get'
+        response = self.routes_playwright.html(url)
+        assert type(response) is HTMLResponse
+        assert response.status_code == 200
+        assert response.media_type  == "text/html"
+        assert '"url": "https://httpbin.org/get"' in bytes_to_str(response.body)
+
 
     def test_screenshot(self):
         #png_signature      = b'\x89PNG\r\n\x1A\n'
