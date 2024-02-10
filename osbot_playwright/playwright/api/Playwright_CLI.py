9e8c2edbd8efe5995939a531aeebbcf7d2660d0f
Dinis Cruz <dinis.cruz@owasp.org>
2024-02-10 01:20:03 +0000

    updated playwright release number

--- osbot_playwright/playwright/api/Playwright_CLI.py
+++ osbot_playwright/playwright/api/Playwright_CLI.py
@@ -7,7 +7,7 @@ from osbot_utils.utils.Process import exec_process
 
 
 FOLDER_NAME_PLAYWRIGHT_BROWSERS = "osbot_playwright_browsers"
-VERSION_PLAYWRIGHT              = "Version 1.40.0"
+VERSION_PLAYWRIGHT              = "Version 1.41.2"
 
 class Playwright_CLI:
 
